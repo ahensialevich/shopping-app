5cc77cd67134c0328c2330a67b66d37ddb1a74d6
Andrei Hensialevich <sixpackgods@gmail.com>
2017-09-28 13:37:00 +0200

    Basic Setup of Components is Finished

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a  test', 'https://static.tumblr.com/e4425b81bfd1a22f908322d0bab7f1ce/jrga6uw/2H0nbc3kx/tumblr_static_tumblr_static_cikl62uwv20ws8soo084gco40_640.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
