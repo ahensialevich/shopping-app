d28ce170e2d4033e47b4dc03e189571be5e970af
Andrei Hensialevich <sixpackgods@gmail.com>
2017-10-17 11:19:38 +0200

    Using a Service for Cross-Component Communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Recipe } from '../recipe.model';
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {Recipe} from '../recipe.model';
+import {Component, OnInit} from '@angular/core';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,21 +8,12 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    // tslint:disable-next-line:max-line-length
-    new Recipe('A Test Recipe', 'This is simply a  test', 'https://static.tumblr.com/e4425b81bfd1a22f908322d0bab7f1ce/jrga6uw/2H0nbc3kx/tumblr_static_tumblr_static_cikl62uwv20ws8soo084gco40_640.jpg'),
-    // tslint:disable-next-line:max-line-length
-    new Recipe('Another Test Recipe', 'This is another simply test', 'http://s10.favim.com/orig/160622/food-junk-food-pizza-tumblr-Favim.com-4441580.jpeg'),
-    // tslint:disable-next-line:max-line-length
-    new Recipe('Yet Another Test Recipe', 'Yet another simply test', 'http://68.media.tumblr.com/8edc19a8a8088529b73c93940c9569e0/tumblr_n9tjz0zMIi1qb90noo1_500.jpg')
-  ];
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  constructor() { }
+  recipes: Recipe[];
 
-  ngOnInit() {
+  constructor(private recipeService: RecipeService) {
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
