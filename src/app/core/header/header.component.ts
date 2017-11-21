eed124edf6bcf1480f85f442ac89b7bf85988c4c
Andrei Hensialevich <sixpackgods@gmail.com>
2017-11-21 14:40:12 +0100

    Application finished

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
-import {DataStorageService} from '../shared/data-storage.service';
+import {DataStorageService} from '../../shared/data-storage.service';
 import {HttpResponse} from '@angular/common/http';
-import {AuthService} from '../auth/auth.service';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -25,5 +25,9 @@ export class HeaderComponent {
 
   onLogout() {
     this.authService.logout();
+  }
+
+  isAuthenticatedMethod() {
+    return this.authService.isAuthenticated();
   }
 }
